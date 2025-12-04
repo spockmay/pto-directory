59c2ea9bcdd63e55c98fba992436f9c895098147
Ryan May <spockmay@users.noreply.github.com>
2025-12-03 21:49:08 -0500

    Enable send_email to include attachments

--- util/send_email.py
+++ util/send_email.py
@@ -1,9 +1,15 @@
 import smtplib
 import configparser
+
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 
-def send_mail(text: str, subject: str, to: str, secrets: dict) -> bool:
+def send_mail(
+    text: str, subject: str, to: str, secrets: dict, filenames=[]
+) -> bool:
     """
     Send an email from a Gmail account.
 
@@ -15,13 +21,34 @@ def send_mail(text: str, subject: str, to: str, secrets: dict) -> bool:
     :type to: str
     :param secrets: a dict with from_address, user, password keys
     :type secrets: dict
+    :param filenames: a list of files to attach to the email
+    :type filenames: list
     :return: success sending the email
     :rtype: bool
     """
-    msg = MIMEText(text, "html")
+    msg = MIMEMultipart()
     msg["From"] = secrets["from_address"]
     msg["To"] = to
     msg["Subject"] = subject
+
+    msg.attach(MIMEText(text, "plain"))
+
+    # attach any files
+    for filename in filenames:
+        attachment = open(filename, "rb")
+        clean = filename.split("/")[-1]
+
+        p = MIMEBase("application", "octet-stream")
+
+        # To change the payload into encoded form
+        p.set_payload(attachment.read())
+
+        encoders.encode_base64(p)
+
+        p.add_header("Content-Disposition", "attachment; filename= %s" % clean)
+
+        # attach the instance 'p' to instance 'msg'
+        msg.attach(p)
 
     gmail_user = secrets["user"]
     gmail_password = secrets["password"]
